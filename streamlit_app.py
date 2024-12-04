64102e6701640becde44380775760f352f8e7a90
Deep Patel <80269801+DPatel03@users.noreply.github.com>
2024-12-04 15:35:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,7 +78,7 @@ st.markdown(
         color: white;
         font-size: 4rem;
         font-weight: bold;
-        margin-top: 20px;
+        margin-top: 0px;
     }
     .subtitle {
         text-align: center;

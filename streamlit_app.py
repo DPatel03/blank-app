a177b9c637248ff8fb3f3325b270b2df391b8c00
dhrlm <117092024+dhrlm@users.noreply.github.com>
2024-12-06 10:27:47 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -164,7 +164,7 @@ st.markdown(
 )
 
 # ---------------------------- HEADER ----------------------------
-st.markdown('<div class="title">💤Predict</div>', unsafe_allow_html=True)
+st.markdown('<div class="title">💤 ZzzPredict 💤</div>', unsafe_allow_html=True)
 st.markdown('<div class="subtitle">A Sleep Quality Prediction Model</div>', unsafe_allow_html=True)
 
 # ---------------------------- FORM ----------------------------
